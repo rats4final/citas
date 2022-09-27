4f07433c1b610e12ee525ea9e89672535d32d722
rats4final <80012704+rats4final@users.noreply.github.com>
2022-09-26 23:32:09 -0400

    Sass, tailwind y vite ejecuten npm install

---
+++ resources/views/test.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="es-BO">
+<head>
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+    <title>Test Page</title>
+</head>
+<body>
+
+<h1 class="bg-fuchsia-300">My First Heading</h1>
+<p>My first paragraph.</p>
+
+</body>
+</html>
