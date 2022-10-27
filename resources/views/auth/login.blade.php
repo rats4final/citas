e8118b47578bf37c26ca3b1d33adc6fc92b61daa
rats4final <80012704+rats4final@users.noreply.github.com>
2022-10-27 00:50:42 -0400

    login mamado

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,8 +1,51 @@
 <x-layouts.guest title="Login" keywords="Login" meta-description="Pagina de Login">
-
-    <div>
-        
+    <div class="min-h-screen bg-hospital bg-no-repeat bg-cover py-6 flex flex-col justify-center sm:py-12">
+        <form action="{{ route('login') }}" method="POST" autocomplete="">
+            @csrf
+            <div class="relative py-3 sm:max-w-xl sm:mx-auto">
+                <div
+                    class="absolute inset-0 bg-gradient-to-r from-blue-300 to-blue-600 shadow-lg transform -skew-y-6 sm:skew-y-0 sm:-rotate-6 sm:rounded-3xl">
+                </div>
+                <div class="relative px-4 py-10 bg-white shadow-lg sm:rounded-3xl sm:p-20">
+                    <div class="max-w-md mx-auto">
+                        <div>
+                            <h1 class="text-2xl font-semibold">Login</h1>
+                        </div>
+                        <div class="divide-y divide-gray-200">
+                            <div class="py-8 text-base leading-6 space-y-4 text-gray-700 sm:text-lg sm:leading-7">
+                                <div class="relative">
+                                    <input autocomplete="off" id="email" name="email" type="email"
+                                        class="peer placeholder-transparent h-10 w-full border-b-2 border-gray-300 text-gray-900 focus:outline-none focus:borer-rose-600"
+                                        placeholder="Email address" />
+                                    <label for="email"
+                                        class="absolute left-0 -top-3.5 text-gray-600 text-sm peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-440 peer-placeholder-shown:top-2 transition-all peer-focus:-top-3.5 peer-focus:text-gray-600 peer-focus:text-sm">Email
+                                    </label>
+                                </div>
+                                <div class="relative">
+                                    <input autocomplete="off" id="password" name="password" type="password"
+                                        class="peer placeholder-transparent h-10 w-full border-b-2 border-gray-300 text-gray-900 focus:outline-none focus:borer-rose-600"
+                                        placeholder="Password" />
+                                    <label for="password"
+                                        class="absolute left-0 -top-3.5 text-gray-600 text-sm peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-440 peer-placeholder-shown:top-2 transition-all peer-focus:-top-3.5 peer-focus:text-gray-600 peer-focus:text-sm">Contraseña</label>
+                                </div>
+                                <div class="relative">
+                                    <button class="bg-blue-500 text-white rounded-md px-2 py-1">Ingresar</button>
+                                </div>
+                                <div class="relative">
+                                    <input type="checkbox" name="remember" id="remember">
+                                    <label for="remember">Recuerdame</label>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </form>
     </div>
-
-
+    @error('email')
+        {{ $message }}
+    @enderror
+    @error('password')
+        {{ $message }}
+    @enderror
 </x-layouts.guest>
--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,10 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('home', function () {
+    return view('home');
+});
+
 Route::get('test', function (){
     return view('test');
 });
