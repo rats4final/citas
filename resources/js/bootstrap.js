d9e30b2b361905f000ba257971de3d0ebd72872c
rats4final <80012704+rats4final@users.noreply.github.com>
2022-10-26 22:49:07 -0400

    arreglos

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -1,3 +1,4 @@
+/*jshint esversion: 6 */
 import _ from 'lodash';
 window._ = _;
 
--- resources/views/test.blade.php
+++ resources/views/test.blade.php
@@ -1,23 +1,33 @@
 <!DOCTYPE html>
 <html lang="es-BO">
+
 <head>
     @vite(['resources/css/app.css', 'resources/js/app.js'])
     <title>Test Page</title>
 </head>
+
 <body>
-<div class="flex">
-    <div class="p-3 text-white font-mono flex-auto h-14 bg-blue-500 rounded text-center m-2">01</div>
-    <div class="p-3 text-white font-mono flex-none h-14 w-80 bg-blue-500 rounded text-center m-2">02</div>
-    <div class="antialiased p-3 text-white font-mono flex-auto h-14 bg-blue-500 rounded text-center m-2">03</div>
-</div>
-<p class="leading-loose m-2 whitespace-normal rounded-lg border-2 p-3 border-blue-900 shadow-2xl italic">
-    "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores doloribus excepturi minima nemo numquam quae
-    sint veritatis. Ab accusantium animi atque debitis delectus deleniti deserunt dolor ducimus eligendi esse facilis
-    hic illum ipsa itaque iure laborum, mollitia nam neque nisi numquam officia omnis optio pariatur porro quaerat quasi
-    quo quos repellat repudiandae sed suscipit tempore temporibus totam vel velit vero voluptate. Ab aliquid amet animi
-    at delectus dolorum enim eum eveniet id illo incidunt ipsa iure libero modi, mollitia natus nobis non, odit optio
-    quaerat quasi, quidem rem reprehenderit repudiandae rerum sequi sint totam vero voluptas voluptatibus! Hic, nemo"
-    numquam.
-</p>
+    <div class="flex">
+        <div class="p-3 text-white font-mono flex-auto h-14 bg-blue-500 rounded text-center m-2">01</div>
+        <div class="p-3 text-white font-mono flex-none h-14 w-80 bg-blue-500 rounded text-center m-2">02</div>
+        <div class="antialiased p-3 text-white font-mono flex-auto h-14 bg-blue-500 rounded text-center m-2">03</div>
+    </div>
+    <p class="leading-loose m-2 whitespace-normal rounded-lg border-2 p-3 border-blue-900 shadow-2xl italic">
+        "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores doloribus excepturi minima nemo numquam
+        quae
+        sint veritatis. Ab accusantium animi atque debitis delectus deleniti deserunt dolor ducimus eligendi esse
+        facilis
+        hic illum ipsa itaque iure laborum, mollitia nam neque nisi numquam officia omnis optio pariatur porro quaerat
+        quasi
+        quo quos repellat repudiandae sed suscipit tempore temporibus totam vel velit vero voluptate. Ab aliquid amet
+        animi
+        at delectus dolorum enim eum eveniet id illo incidunt ipsa iure libero modi, mollitia natus nobis non, odit
+        optio
+        quaerat quasi, quidem rem reprehenderit repudiandae rerum sequi sint totam vero voluptas voluptatibus! Hic,
+        nemo"
+        numquam.
+    </p>
+   
 </body>
+
 </html>
