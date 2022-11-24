832d9b23e5dbcc98126a7836d6ebc0c8b111fd63
Marc15975 <91290441+Marc15975@users.noreply.github.com>
2022-11-24 03:52:56 -0400

    Sedes y Historial

--- resources/views/admin/Sedes/create.blade.php
+++ resources/views/admin/Sedes/create.blade.php
@@ -3,18 +3,18 @@
         <div class="card-header">Registro de Sedes</div>
         <div class="card-body">
 
-            <form action="{{ url('sedes') }}" method="post">
+            <form action="{{ url('sedes') }}" class="was-validated" method="post">
               {!! csrf_field() !!}
               <label>Nombre sede</label></br>
-              <input type="text" name="nombre_sede" id="nombre_sede" class="form-control"></br>
+              <input type="text" name="nombre_sede" id="nombre_sede" class="form-control" required></br>
               <label>Direccion de sede</label></br>
-              <input type="text" name="direccion_sede" id="direccion_sede" class="form-control"></br>
+              <input type="text" name="direccion_sede" id="direccion_sede" class="form-control" required></br>
               <label>Telefono de la Sede</label></br>
-              <input type="text" name="telefono_sede" id="telefono_sede" class="form-control"></br>
+              <input type="text" name="telefono_sede" id="telefono_sede" class="form-control" required></br>
 
               <label>Ciudad Sede</label></br>
-              <select class="form-select"  name="ciudad_sede" id="ciudad_sede"  aria-label="Default select example">
-                <option selected>Selecione la ciudad</option>
+              <select class="form-select"  name="ciudad_sede" id="ciudad_sede" required aria-label="Default select example">
+                <option selected value="">Selecione la ciudad</option>
                 <option value="La Paz">La Paz</option>
                 <option value="Santa Cruz">Santa cruz</option>
                 <option value="Cochabamba">Cochabamba</option>
