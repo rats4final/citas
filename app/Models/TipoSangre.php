ed3a2ddd913b7fe1671c539e8a67876457e4eea1
rats4final <80012704+rats4final@users.noreply.github.com>
2022-11-24 15:21:51 -0400

    old tipos sangre

--- app/Models/TipoSangre.php
+++ app/Models/TipoSangre.php
@@ -9,7 +9,7 @@ class TipoSangre extends Model
 {
     use HasFactory;
 
-    protected $table = 'tipo_sangre';
+    protected $table = 'tipos_sangre';
     protected $primaryKey = 'id_tipo_sangre';
     protected $fillable = ['nombre_tipo_sangre','descripcion_tipo_sangre','rareza_tipo_sangre'];
 
--- resources/views/admin/tipo_relacion/index.blade.php
+++ resources/views/admin/tipo_relacion/index.blade.php
@@ -7,7 +7,8 @@
                         <h2>Tipo Relacion</h2>
                     </div>
                     <div class="card-body">
-                        <a href="{{ route('TipoRelacion.create') }}" class="btn btn-success btn-sm" title="Añadir nueva Persona">
+                        <a href="{{ route('TipoRelacion.create') }}" class="btn btn-success btn-sm"
+                            title="Añadir nueva Persona">
                             <i class="fa fa-plus" aria-hidden="true"></i> Nueva Relacion
                         </a>
                         <br>
@@ -16,41 +17,42 @@
                             <table class="table">
                                 <caption>Todas la relaciones</caption>
                                 <thead>
-                                <tr>
-                                    <th>Relacion</th>
-                                    <th>Persona 1</th>
-                                    <th>Persona 2</th>
-                                    <th>Acciones</th>
-                                </tr>
+                                    <tr>
+                                        <th>Relacion</th>
+                                        <th>Persona 1</th>
+                                        <th>Persona 2</th>
+                                        <th>Acciones</th>
+                                    </tr>
                                 </thead>
                                 <tbody>
-                                @foreach ($TipoRelaciones as $TipoRelacion)
-                                    <tr>
-                                        <td>{{ $TipoRelacion->relacion }}</td>
-                                        <td>{{ $TipoRelacion->persona->nombre_per }}</td>
-                                        <td>{{ $TipoRelacion->pariente->nombre_per }}</td>
-                                        <td>
-                                            <a href="{{ route('TipoRelacion.show', $TipoRelacion) }}"
-                                               title="View Student"><button class="btn btn-info btn-sm"><i
-                                                        class="fa fa-eye" aria-hidden="true"></i>
-                                                    Mostrar</button></a>
-                                            <a href="{{ route('TipoRelacion.edit', $TipoRelacion) }}"
-                                               title="Edit Student"><button class="btn btn-primary btn-sm"><i
-                                                        class="fa fa-pencil-square-o" aria-hidden="true"></i>
-                                                    Editar</button></a>
-                                            <form method="POST" action="{{ route('TipoRelacion.destroy', $TipoRelacion) }}"
-                                                  accept-charset="UTF-8" style="display:inline">
-                                                @method('DELETE')
-                                                @csrf
-                                                <button type="submit" class="btn btn-danger btn-sm"
+                                    @foreach ($TipoRelaciones as $TipoRelacion)
+                                        <tr>
+                                            <td>{{ $TipoRelacion->relacion }}</td>
+                                            <td>{{ $TipoRelacion->persona->nombre_per }}</td>
+                                            <td>{{ $TipoRelacion->pariente->nombre_per }}</td>
+                                            <td>
+                                                <a href="{{ route('TipoRelacion.show', $TipoRelacion) }}"
+                                                    title="View Student"><button class="btn btn-info btn-sm"><i
+                                                            class="fa fa-eye" aria-hidden="true"></i>
+                                                        Mostrar</button></a>
+                                                <a href="{{ route('TipoRelacion.edit', $TipoRelacion) }}"
+                                                    title="Edit Student"><button class="btn btn-primary btn-sm"><i
+                                                            class="fa fa-pencil-square-o" aria-hidden="true"></i>
+                                                        Editar</button></a>
+                                                <form method="POST"
+                                                    action="{{ route('TipoRelacion.destroy', $TipoRelacion) }}"
+                                                    accept-charset="UTF-8" style="display:inline">
+                                                    @method('DELETE')
+                                                    @csrf
+                                                    <button type="submit" class="btn btn-danger btn-sm"
                                                         title="Delete Student"
                                                         onclick="return confirm(&quot;Confirm delete?&quot;)"><i
-                                                        class="fa fa-trash-o" aria-hidden="true"></i>
-                                                    Borrar</button>
-                                            </form>
-                                        </td>
-                                    </tr>
-                                @endforeach
+                                                            class="fa fa-trash-o" aria-hidden="true"></i>
+                                                        Borrar</button>
+                                                </form>
+                                            </td>
+                                        </tr>
+                                    @endforeach
                                 </tbody>
                             </table>
                         </div>
